bfe5aa2ccd9566c9b8d60023e264db3c0cb56970
digbose92 <digbose92@gmail.com>
2023-01-23 14:52:37 -0800

    Added updates to BN tokens

--- vilt/modules/__init__.py
+++ vilt/modules/__init__.py
@@ -1,3 +1,5 @@
 from .vilt_module import ViLTransformerSS
 from .vilt_module import ViLTTransformerBN
 from .vilt_module import ViLTTransformerBNv2
+from .vilt_module import ViLTTransformerBN_token_average
+
