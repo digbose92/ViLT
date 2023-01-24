881e1109a711e950167854fe6779759fa9a74356
digbose92 <digbose92@gmail.com>
2023-01-24 01:26:27 -0800

    Added updates to vilt module for token averaging

--- vilt/modules/__init__.py
+++ vilt/modules/__init__.py
@@ -2,4 +2,5 @@ from .vilt_module import ViLTransformerSS
 from .vilt_module import ViLTTransformerBN
 from .vilt_module import ViLTTransformerBNv2
 from .vilt_module import ViLTTransformerBN_token_average
+from .vilt_module import ViLTTransformer_token_aggregate
 
