2b6219ad7fee25c77eddfb4461d9c135612aa0f2
digbose92 <digbose92@gmail.com>
2023-01-31 01:42:17 -0800

    Added updates to vilt for bn free attention across tokens

--- vilt/modules/__init__.py
+++ vilt/modules/__init__.py
@@ -1,7 +1,7 @@
 from .vilt_module import ViLTransformerSS
 from .vilt_module import ViLTTransformerBN
 from .vilt_module import ViLTTransformerBNv2
-from .vilt_module import ViLTTransformerBN_token_average
 from .vilt_module import ViLTTransformer_token_aggregate
 from .vilt_module import ViLTTransformerBN_modality_token_average
+from .vilt_module import ViLTTransformerBN_free_attention_modality_token_average
 
